b222a8c3134248fc5b1627b18c623e5a8fec6104
nori11nea-maker <nori11.nea@gmail.com>
2026-05-31 01:10:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ app.get("/", (req, res) => {
   res.send("Pingis backend kör");
 });
 
-// VIKTIGT: Railway kräver denna
-const PORT = process.env.PORT;
+// VIKTIGT: fallback så den ALDRIG kraschar
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Backend kör på port", PORT);
