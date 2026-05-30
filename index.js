9d939a9d7448c22508202d09ef5e9745780a17e7
nori11nea-maker <nori11.nea@gmail.com>
2026-05-31 00:58:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("backend startar");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Pingis backend kör");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Backend kör på port", PORT);
+});
