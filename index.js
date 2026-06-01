ca7ad863e511fa0b79fd8af20e5df8a9f01622f7
nori11nea-maker <nori11.nea@gmail.com>
2026-06-02 00:16:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 import express from "express";
+import { createClient } from "@supabase/supabase-js";
 
 const app = express();
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
 
 app.get("/", (req, res) => {
   res.send("Pingis backend kör");
 });
 
-// VIKTIGT: fallback så den ALDRIG kraschar
+app.get("/players", async (req, res) => {
+  const { data, error } = await supabase
+    .from("players")
+    .select("*");
+
+  if (error) {
+    return res.status(500).json(error);
+  }
+
+  res.json(data);
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
